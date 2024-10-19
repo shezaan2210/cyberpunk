dcf51272be1ef9eb332607f9ae8aeceda940c05a
shezaan <shezaanansari2210@gmail.com>
2024-10-20 05:17:34 +0530

    cyber

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ camera.position.z = 3.5;
 // mouse movements
 window.addEventListener('mousemove', (e)=>{
     if(model){
-        const rotationX = (e.clientX / window.innerWidth - .5) * (Math.PI * .3)
-        const rotationY = (e.clientY / window.innerHeight - .5) * (Math.PI * .3)
+        const rotationX = (e.clientX / window.innerWidth - .5) * (Math.PI * .13)
+        const rotationY = (e.clientY / window.innerHeight - .5) * (Math.PI * .13)
         gsap.to(model.rotation, {
             y: rotationX,
             x: rotationY,
